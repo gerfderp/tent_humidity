b923f7f7f540da29c710a424d45f0a14fbd8bca7
Tara Neier <tara@neier.org>
2017-08-04 18:13:51 -0700

    working with DHT22 and LED backpack

--- tent_humidity.ino
+++ tent_humidity.ino
@@ -1,38 +1,55 @@
 
-#include "DHT.h"
 
-#define DHTPIN 2     // what pin we're connected to
+// include the library code
+#include "Adafruit_LEDBackpack.h"
+#include <Adafruit_GFX.h>
+#include <TinyDHT.h>
+#include <TinyWireM.h> // Wire/I2C library for Trinket and Gemma
+#include <avr/power.h> // needed to up clock to 16 MHz on 5v Trinket
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTTYPE DHT22
+#define TEMPTYPE 1
 
+// Trinket GPIO #1 would be better but LED prevents digital talk with DHT sensor
+#define DHTPIN 3 // Sensor connected to GPIO #3
 #define FANPIN 4
-#define BLUE 3
-DHT dht(DHTPIN, DHTTYPE);                     
+DHT dht(DHTPIN, DHTTYPE); // Define Temp Sensor
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
 void setup() {
-   dht.begin();
+  if (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1); // 5V Trinket: run at 16 MHz
+  dht.begin();                       // Initialize DHT Teperature Sensor
+  matrix.begin(0x70);
+  matrix.print(0xBEE5, HEX);
+  matrix.writeDisplay();
   pinMode(FANPIN, OUTPUT);
-   pinMode(BLUE, OUTPUT);
-  // Set the pin high as the default state
-  digitalWrite(FANPIN, HIGH); 
-  delay(500);
-  digitalWrite(FANPIN, LOW); 
-    digitalWrite(BLUE, HIGH); 
-delay(500);
-  digitalWrite(BLUE, LOW);
+  digitalWrite(FANPIN, HIGH);
+  delay(3000);
+  digitalWrite(FANPIN, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  float t = dht.readTemperature(true);
-//  t = 60;
-  if ( t > 75) {
-    digitalWrite(FANPIN, HIGH);   
-    digitalWrite(BLUE, LOW);
+  int8_t h = dht.readHumidity();             // Read humidity
+  int16_t t = dht.readTemperature(TEMPTYPE); // read temperature
+  if (t == BAD_TEMP || h == BAD_HUM || t == 32 ||
+      h == 0) { // if error conditions (see TinyDHT.h)
+    matrix.print(0xEEEE, HEX);
+    matrix.writeDisplay();
   } else {
-    digitalWrite(BLUE, HIGH);
-    digitalWrite(FANPIN, LOW); 
+    matrix.print(t);
+    matrix.drawColon(true);
+    matrix.writeDisplay();
+    delay(15000);
+    matrix.print(h);
+    matrix.drawColon(false);
+    matrix.writeDisplay();
+    if (h > 55) {
+      digitalWrite(FANPIN, HIGH);
+    } else {
+      digitalWrite(FANPIN, LOW);
+    }
   }
-  delay(2000);
+  delay(15000);
 }
